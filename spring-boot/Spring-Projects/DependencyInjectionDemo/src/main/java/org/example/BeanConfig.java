6db1a9b763301895e63693de32fbb499e453460f
Ghazi Khan <mailme.gkhan@gmail.com>
2024-01-11 15:40:12 +0530

    dependencyInjectionDemo - added java base DI - 2

--- spring-boot/Spring-Projects/DependencyInjectionDemo/src/main/java/org/example/BeanConfig.java
+++ spring-boot/Spring-Projects/DependencyInjectionDemo/src/main/java/org/example/BeanConfig.java
@@ -2,12 +2,14 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @Configurable
+@ComponentScan(basePackages = "org.example")
 public class BeanConfig {
 
-    @Bean
-    public Principal principal() {
-        return new Principal();
-    }
+//    @Bean
+//    public Principal principal() {
+//        return new Principal();
+//    }
 }

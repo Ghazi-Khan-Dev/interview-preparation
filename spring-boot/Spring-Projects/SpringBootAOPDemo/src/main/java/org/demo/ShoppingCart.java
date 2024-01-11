8cfaeb2f0e2a9bdbb4e04d9618beefe4a8563500
Ghazi Khan <mailme.gkhan@gmail.com>
2024-01-11 18:37:25 +0530

    SpringBootAOPDemo - created new project

---
+++ spring-boot/Spring-Projects/SpringBootAOPDemo/src/main/java/org/demo/ShoppingCart.java
@@ -0,0 +1,14 @@
+package org.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+
+    public void checkOut() {
+        // Logging
+        // Authentication & Authorization
+        // Sanitize the data
+        System.out.println("checkout is called..");
+    }
+}
